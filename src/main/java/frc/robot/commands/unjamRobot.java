431ccc09fd4f9f3334969fc109e1ee09f9b1c5cb
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2026-03-02 16:18:39 -0700

    Unjam.

--- src/main/java/frc/robot/commands/unjamRobot.java
+++ src/main/java/frc/robot/commands/unjamRobot.java
@@ -4,10 +4,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Indexer;
 
-public class unjamIntake extends Command {
+public class unjamRobot extends Command {
     Hopper hopper;
     Indexer indexer;
-    public unjamIntake(Hopper hopper, Indexer indexer){
+    public unjamRobot(Hopper hopper, Indexer indexer){
         this.hopper = hopper;
         this.indexer = indexer;
         addRequirements(hopper,indexer);
@@ -15,5 +15,16 @@ public class unjamIntake extends Command {
 
     @Override
     public void execute() {
+        indexer.setRPM(-2500);
+        hopper.setRPM(-1000);
+    }
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    @Override
+    public void end(boolean interrupted){
+        indexer.stop();
+        hopper.stop();
     }
 }
